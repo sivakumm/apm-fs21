165c96b60d6d2f8b1a9ed2dec3cba48a502f5811
Zoltán Majó <zoltan.majo@ergon.ch>
2021-06-06 15:04:17 +0200

    add code

---
+++ week-13/code/Classloader/src/apm/classloader/Singleton.java
@@ -0,0 +1,16 @@
+package apm.classloader;
+
+public class Singleton {
+	private static Singleton instance = new Singleton();
+	
+	private Singleton() {
+		System.out.println("Singleton instance initialized!");
+	}
+	
+	public static Singleton getInstance() {
+		return instance;
+	}
+	
+	public void m() {
+	}
+}
